3d5aae6e7708c818c9dae4276f260731bd33df37
Tokay Korkut <tokaykorkut@gmail.com>
2021-09-05 00:43:36 +0300

    base structures are cleaned up

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+
+  await app.listen(process.env.SERVER_PORT || 4000);
 }
 
 bootstrap();
