79750ebb208aa0be46ce575372058f217ea98d55
Tokay Korkut <tokaykorkut@gmail.com>
2021-09-08 00:32:37 +0300

    cors was disabled

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors: true});
 
   await app.listen(process.env.SERVER_PORT || 4000);
 }
